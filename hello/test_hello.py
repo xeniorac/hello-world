a72777b1e949e911dce82efdd8c4fee51b39d990
Vishanth Bharadwaj <vishanthbharadwaj@gmail.com>
2020-05-26 11:03:42 -0700

    Update test_hello.py

--- hello/test_hello.py
+++ hello/test_hello.py
@@ -9,8 +9,8 @@ class TestHello(unittest.TestCase):
 
     def test_world(self):
         """sample test"""
-        self.assertEqual(hello('world'), 'hello world')
+        self.assertEqual(hello('worldvb'), 'hello worldvb')
 
     def test_world_unicode(self):
         """sample test with unicode"""
-        self.assertEqual(hello(u'world'), u'hello world')
+        self.assertEqual(hello(u'worldvb'), u'hello worldvb')
